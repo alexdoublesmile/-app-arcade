75a0ad7911e53a8064d7be4862ef768fcd3c8aae
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 15:45:04 +0300

    add bullet init

--- src/main/java/com/joyful/arcade/Bullet.java
+++ src/main/java/com/joyful/arcade/Bullet.java
@@ -11,4 +11,16 @@ public class Bullet {
     private double dx;
     private double dy;
     private Color color1;
+
+    public Bullet(double angle, int x, int y) {
+        this.x = x;
+        this.y = y;
+        r = 2;
+        rad = Math.toRadians(angle);
+        dx = Math.cos(rad);
+        dy = Math.sin(rad);
+        speed = 15;
+        
+        color1 = Color.YELLOW
+    }
 }
