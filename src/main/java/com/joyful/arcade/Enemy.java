1400bc201b38ded82bd9e0009ba9299bd672c0da
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 16:17:57 +0300

    add enemy

---
+++ src/main/java/com/joyful/arcade/Enemy.java
@@ -0,0 +1,23 @@
+package com.joyful.arcade;
+
+import java.awt.*;
+
+public class Enemy {
+    private double x;
+    private double y;
+    private int r;
+
+    private double dx;
+    private double dy;
+    private double rad;
+    private double speed;
+
+    private int health;
+    private int type;
+    private int rank;
+
+    private Color color1;
+
+    private boolean ready;
+    private boolean dead;
+}
