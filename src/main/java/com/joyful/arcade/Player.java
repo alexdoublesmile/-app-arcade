aeedd36be5df76321058206f2f1c38440970eae4
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 14:57:39 +0300

    add player

---
+++ src/main/java/com/joyful/arcade/Player.java
@@ -0,0 +1,11 @@
+package com.joyful.arcade;
+
+public class Player {
+    private int x;
+    private int y;
+    private int r;
+    private int dx;
+    private int dy;
+    private int speed;
+    private int lives;
+}
