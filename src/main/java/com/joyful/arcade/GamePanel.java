93b9a5fae3d75818d6e721ec3b32e282941b9278
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 14:33:17 +0300

    add main panel settings

--- src/main/java/com/joyful/arcade/GamePanel.java
+++ src/main/java/com/joyful/arcade/GamePanel.java
@@ -3,13 +3,30 @@ package com.joyful.arcade;
 import javax.swing.*;
 import java.awt.*;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
     private static final int WIDTH = 400;
     private static final int HEIGHT = 400;
+
+    private Thread thread;
 
     public GamePanel() {
         super();
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setFocusable(true);
+        requestFocus();
+
+    }
+
+    public void addNotify() {
+        super.addNotify();
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    @Override
+    public void run() {
 
     }
 }
