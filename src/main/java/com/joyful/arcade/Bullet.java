35a2eee96ba92fed9aa90f0e73c3907d7a3d85ce
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 15:43:07 +0300

    add bullet

---
+++ src/main/java/com/joyful/arcade/Bullet.java
@@ -0,0 +1,14 @@
+package com.joyful.arcade;
+
+import java.awt.*;
+
+public class Bullet {
+    private double x;
+    private double y;
+    private int r;
+    private double speed;
+    private double rad;
+    private double dx;
+    private double dy;
+    private Color color1;
+}
