7c44574353ae60e853903a2480fa0d3da8ff8b81
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 16:37:16 +0300

    add enemy draw

--- src/main/java/com/joyful/arcade/Enemy.java
+++ src/main/java/com/joyful/arcade/Enemy.java
@@ -89,6 +89,13 @@ public class Enemy {
     }
 
     public void draw(Graphics2D g) {
+        g.setColor(color1);
+        g.fillOval((int) x - r, (int) y - r, 2 * r, 2 * r);
 
+        g.setStroke(new BasicStroke(3));
+        g.setColor(color1.darker());
+        g.drawOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+
+        g.setStroke(new BasicStroke(1));
     }
 }
