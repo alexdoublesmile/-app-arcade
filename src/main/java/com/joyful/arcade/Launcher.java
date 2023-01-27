5b0772481a23f53b2f74902bf4f3a39fba5b71ca
Александр Плохой <alexplohoy@mail.ru>
2023-01-27 14:28:40 +0300

    add main frame

--- src/main/java/com/joyful/arcade/Launcher.java
+++ src/main/java/com/joyful/arcade/Launcher.java
@@ -1,7 +1,15 @@
 package com.joyful.arcade;
 
+import javax.swing.*;
+
 public class Launcher {
     public static void main(String[] args) {
+        final JFrame window = new JFrame("First game");
+        window.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        window.setContentPane(new GamePanel());
+
+        window.pack();
+        window.setVisible(true);
     }
 }
