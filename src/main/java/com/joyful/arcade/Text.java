0a88b5fb97ad22fcfaab87470ff736bb361311d1
Александр Плохой <alexplohoy@mail.ru>
2023-01-28 08:23:22 +0300

    add text class

---
+++ src/main/java/com/joyful/arcade/Text.java
@@ -0,0 +1,17 @@
+package com.joyful.arcade;
+
+public class Text {
+    private double x;
+    private double y;
+    private long time;
+    private String s;
+
+    private long start;
+
+    public Text(double x, double y, long time, String s) {
+        this.x = x;
+        this.y = y;
+        this.time = time;
+        this.s = s;
+    }
+}
