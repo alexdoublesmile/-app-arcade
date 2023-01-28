74681ca7c25100d918024dd5bf8c789a283983d6
Александр Плохой <alexplohoy@mail.ru>
2023-01-28 09:03:55 +0300

    fix enemy powers

--- src/main/java/com/joyful/arcade/Enemy.java
+++ src/main/java/com/joyful/arcade/Enemy.java
@@ -87,22 +87,22 @@ public class Enemy {
             if (rank == 1) {
                 speed = 1.5;
                 r = 5;
-                health = 5;
+                health = 3;
             }
             if (rank == 2) {
                 speed = 1.5;
                 r = 10;
-                health = 6;
+                health = 4;
             }
             if (rank == 3) {
                 speed = 1.5;
                 r = 25;
-                health = 7;
+                health = 5;
             }
             if (rank == 4) {
                 speed = 1.5;
                 r = 45;
-                health = 8;
+                health = 5;
             }
         }
 
