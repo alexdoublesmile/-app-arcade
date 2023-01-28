b405f69b8b94623376ec09beb332597d0ae884aa
Александр Плохой <alexplohoy@mail.ru>
2023-01-28 07:12:22 +0300

    add explosion entity

---
+++ src/main/java/com/joyful/arcade/Explosion.java
@@ -0,0 +1,16 @@
+package com.joyful.arcade;
+
+public class Explosion {
+
+    private double x;
+    private double y;
+    private int r;
+    private int maxRadius;
+
+    public Explosion(double x, double y, int r, int maxRadius) {
+        this.x = x;
+        this.y = y;
+        this.r = r;
+        this.maxRadius = maxRadius;
+    }
+}
