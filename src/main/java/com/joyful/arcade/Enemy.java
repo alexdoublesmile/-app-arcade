4a70d6a0f78c6b88ea2576d00e35276614794be8
Александр Плохой <alexplohoy@mail.ru>
2023-01-28 06:59:23 +0300

    add enemy hit draw

--- src/main/java/com/joyful/arcade/Enemy.java
+++ src/main/java/com/joyful/arcade/Enemy.java
@@ -166,14 +166,27 @@ public class Enemy {
     }
 
     public void draw(Graphics2D g) {
-        g.setColor(color1);
-        g.fillOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+        if (hit) {
 
-        g.setStroke(new BasicStroke(3));
-        g.setColor(color1.darker());
-        g.drawOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+            g.setColor(Color.WHITE);
+            g.fillOval((int) x - r, (int) y - r, 2 * r, 2 * r);
 
-        g.setStroke(new BasicStroke(1));
+            g.setStroke(new BasicStroke(3));
+            g.setColor(Color.WHITE.darker());
+            g.drawOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+
+            g.setStroke(new BasicStroke(1));
+        } else {
+
+            g.setColor(color1);
+            g.fillOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+
+            g.setStroke(new BasicStroke(3));
+            g.setColor(color1.darker());
+            g.drawOval((int) x - r, (int) y - r, 2 * r, 2 * r);
+
+            g.setStroke(new BasicStroke(1));
+        }
     }
 
     public int getType() {
